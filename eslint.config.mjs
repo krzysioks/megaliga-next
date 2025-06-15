b48c5069bf1fa4a709f0bf82981d564d421b4c7d
Krzysztof Pielak <krzysztof.pi@convert.no>
2025-06-15 11:11:18 +0200

    Lint configuration fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,62 +1,79 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import jest from "eslint-plugin-jest";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import jest from 'eslint-plugin-jest';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+    baseDirectory: __dirname
 });
 
 const config = {
-	languageOptions: {
-		parser: tsParser,
-		parserOptions: {
-			ecmaVersion: 6,
-			sourceType: "module",
-		},
-		globals: {
-			...js.configs.recommended.languageOptions.globals,
-			window: "readonly",
-			document: "readonly",
-		},
-	},
-	linterOptions: {
-		reportUnusedDisableDirectives: true,
-	},
-	plugins: {
-		react,
-		jest,
-		"@typescript-eslint": tseslint,
-	},
-	settings: {
-		react: {
-			createClass: "createReactClass",
-			pragma: "React",
-			version: "detect",
-			flowVersion: "0.53",
-		},
-	},
-	rules: {
-		...js.configs.recommended.rules,
-		...react.configs.recommended.rules,
-		...jest.configs.recommended.rules,
-		...tseslint.configs.recommended.rules,
-		"no-prototype-builtins": "off",
-		"no-undef": "off",
-		"no-useless-escape": "off",
-	},
+    languageOptions: {
+        parser: tsParser,
+        parserOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            ecmaFeatures: {
+                jsx: true
+            }
+        },
+        globals: {
+            window: 'readonly',
+            document: 'readonly',
+            console: 'readonly',
+            process: 'readonly',
+            Buffer: 'readonly',
+            __dirname: 'readonly',
+            __filename: 'readonly',
+            module: 'readonly',
+            require: 'readonly',
+            exports: 'readonly',
+            global: 'readonly'
+        }
+    },
+    linterOptions: {
+        reportUnusedDisableDirectives: true
+    },
+    plugins: {
+        react,
+        jest,
+        '@typescript-eslint': tseslint
+    },
+    settings: {
+        react: {
+            createClass: 'createReactClass',
+            pragma: 'React',
+            version: 'detect',
+            flowVersion: '0.53'
+        }
+    },
+    rules: {
+        'no-prototype-builtins': 'off',
+        'no-undef': 'off',
+        'no-useless-escape': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' }
+        ],
+        '@typescript-eslint/no-explicit-any': 'warn'
+    }
 };
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-	...config,
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        ...config
+    }
 ];
 
 export default eslintConfig;
