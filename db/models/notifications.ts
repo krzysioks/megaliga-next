7478270edf052c53e3baa7670e8ed3602cb4491f
krzysioks <krzysiox@o2.pl>
2026-05-26 16:28:26 +0200

    [MN-13] Code clean up

--- db/models/notifications.ts
+++ db/models/notifications.ts
@@ -96,7 +96,7 @@ notificationsSchema.method(
     }
 );
 
-// TODOKP: 1. save notfication document - will be triggered directly on NotificationsModel in place of invocation. const contification = new NotificationsModel({ message: 'New notification', status: 'active', isDissmissible: true }); await notification.save();
+// TODOKP: save notfication document - will be triggered directly on NotificationsModel in place of invocation. const contification = new NotificationsModel({ message: 'New notification', status: 'active', isDissmissible: true }); await notification.save();
 
 const NotificationsModel = model<NotificationsType, NotificationsModelType>(
     'Notifications',
